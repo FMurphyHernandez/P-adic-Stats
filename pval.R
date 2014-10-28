30a79203745d82ae95e87c77314fa66884af3f97
Frank Murphy Hernandez <murphy@ciencias.unam.mx>
2014-10-28 15:19:44 -0600

    Update pval.R

--- pval.R
+++ pval.R
@@ -3,17 +3,25 @@ library('MASS')
 
 pval<-function(x,p){url<-'http://primes.utm.edu/lists/small/10000.txt'
   x1<-MASS:::.rat(x)$rat[1]
-  x2<-MASS:::.rat(x)$rat[1]
-  c1<-x1%p
-  c2<-x2%p
+  x2<-MASS:::.rat(x)$rat[2]
+  c1<-x1%%p
+  c2<-x2%%p
   if(c1!=0 & c2!=0)
   return(0)
   if(c1==0){
-    d1=1
     n=1
     while(c1==0){
       n=n+1
-      c1=x1%(p^n)
+      c1=x1%%(p^n)
       }
+     return(n-1)  
+    }
+  else{
+  n=1
+  while(c2==0){
+      n=n+1
+      c2=x2%%(p^n)
+      }
+     return(-n+1)   
     }
   }
